f3a9daa70502a4939b7f9e9b3e3efba2633969eb
ezekielkibiego <kibiezekiel@gmail.com>
2024-04-18 10:45:17 +0300

    add media queries

--- src/components/CharacterDetails.jsx
+++ src/components/CharacterDetails.jsx
@@ -16,20 +16,21 @@ const CharacterDetails = () => {
   }
 
   return (
-    <div className="flex justify-center items-center h-screen mt-4 mb-4">
-      <div className="max-w-xl w-full bg-white rounded-lg overflow-hidden shadow-lg">
-        <img className="w-full" src={character.image} alt={character.name} />
-        <div className="p-4">
+    <div className="container mx-auto mt-8">
+      <h1 className="text-3xl font-semibold mb-8 text-center">{character.name} Details</h1>
+      <div className="max-w-xl mx-auto bg-white rounded-lg overflow-hidden shadow-lg flex">
+        <img className="w-3/4" src={character.image} alt={character.name} />
+        <div className="p-4 w-1/2">
           <h2 className="text-2xl font-semibold mb-2">{character.name}</h2>
           <p><strong>Status:</strong> {character.status}</p>
           <p><strong>Species:</strong> {character.species}</p>
           <p><strong>Gender:</strong> {character.gender}</p>
         </div>
-        <div className="p-4 flex justify-center">
-          <Link to="/" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Go back home
-          </Link>
-        </div>
+      </div>
+      <div className="flex justify-center mt-4">
+        <Link to="/" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Go back home
+        </Link>
       </div>
     </div>
   );
--- src/components/CharactersList.jsx
+++ src/components/CharactersList.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import './Style.css'
 
 const CharactersList = () => {
   const [characters, setCharacters] = useState([]);
@@ -24,8 +25,8 @@ const CharactersList = () => {
   };
 
   return (
-    <div className="container mx-auto p-8 mb-4 mt-4">
-      <h1 className='mb-4 font-xl text-center '>List of Characters</h1>
+    <div className="container">
+      <h1 className='text-3xl font-semibold mb-8 text-center '>List of Characters</h1>
       <div className="w-full flex justify-center mb-4">
         <input
           type="text"
@@ -35,14 +36,14 @@ const CharactersList = () => {
           className="w-full px-4 py-4 border rounded-md border-red-500"
         />
       </div>
-      <div className="grid grid-cols-3 gap-4">
+      <div className="card-container">
         {filteredCharacters.map(character => (
           <div key={character.id} className="border p-4">
             <Link to={`/character/${character.id}`}>
               <img src={character.image} alt={character.name} className="w-full h-auto mb-2" />
               <p className="text-center">{character.name}</p>
             </Link>
-            <button onClick={() => deleteCharacter(character.id)} className="block w-full mt-2 bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded">
+            <button onClick={() => deleteCharacter(character.id)} className="block w-full mt-2 bg-red-500 hover:bg-red-400 text-white font-bold py-2 px-4 rounded">
               Delete
             </button>
           </div>
